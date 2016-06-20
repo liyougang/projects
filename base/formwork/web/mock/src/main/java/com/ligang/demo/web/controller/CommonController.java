6af4532b02b4988ddb58c3ad278cf085c2f90a46
ligang <huchangligang@163.com>
2016-06-20 19:08:12 +0800

    [base][framework][mock] add bootstrap

--- base/formwork/web/mock/src/main/java/com/ligang/demo/web/controller/CommonController.java
+++ base/formwork/web/mock/src/main/java/com/ligang/demo/web/controller/CommonController.java
@@ -3,6 +3,8 @@ package com.ligang.demo.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 1.0.0 
  */
 @Controller
-public class BasedController {
+public class CommonController {
+	
+	private Logger logger = LoggerFactory.getLogger(CommonController.class);
 	
 	@RequestMapping("/index")
 	public String index(){
-		return "login";
+		logger.info(">>>>>>>>>index");
+		return "index";
 	}
 	@RequestMapping("/login")
 	public String login(HttpServletRequest request,HttpServletResponse resp){
